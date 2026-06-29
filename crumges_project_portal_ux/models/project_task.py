f295cb8a55746fb9a2e140d9e359dc1ca1174d6c
Mario Nuñez <marionumza@gmail.com>
2026-06-28 23:25:02 -0300

    FIX Modules

--- crumges_project_portal_ux/models/project_task.py
+++ crumges_project_portal_ux/models/project_task.py
@@ -16,3 +16,23 @@ class ProjectTask(models.Model):
             'target': 'self',
             'url': self.get_portal_url(),
         }
+
+    def action_send_mail_ux(self):
+        self.ensure_one()
+        template = self.env.ref('crumges_project_portal_ux.email_template_project_task', raise_if_not_found=False)
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'view_id': False,
+            'target': 'new',
+            'context': {
+                'default_model': 'project.task',
+                'default_res_ids': self.ids,
+                'default_template_id': template.id if template else False,
+                'default_composition_mode': 'comment',
+                'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+                'force_email': True,
+            },
+        }
